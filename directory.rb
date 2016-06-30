57309b2035592653dfbec5d50c614971dd71276f
Zak Richardson <Zak@Zaks-MacBook-Pro.local>
2016-06-30 11:52:21 +0100

    Add number of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first print the list of students
 puts "The students of Villains Academy"
 puts "----------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# print the total number of students
+print "Overall, we have "
+#using print as we dont want new lines
+print 11
+puts " great students"
